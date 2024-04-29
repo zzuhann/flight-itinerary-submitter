985c03e4de55af5a49f5702ae60b4db6935208bd
zzuhann <zzuhann0125@gmail.com>
2024-04-30 00:41:06 +0800

    chore: adjust api payload

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,7 +3,7 @@ import { Airport } from './type';
 
 const apis = {
   getAirports: (IATA: string): Promise<Airport[]> => {
-    return instance.get(`/${IATA}`, { params: { $format: 'JSON' } });
+    return instance.get(`/${IATA}`, { params: { $format: 'JSON', $orderby: 'ScheduleDepartureTime' } });
   },
 };
 
